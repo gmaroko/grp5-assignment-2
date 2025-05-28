6254dc49e909bb4251caedbc16fe969eec321aff
Maroko Gideon <marokogideon@gmail.com>
2025-05-28 17:11:13 +0300

    question 2 ii

--- question-2-ii.cpp
+++ question-2-ii.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-  int n;
-  int i;
-  double array[50];
-  cout << "How many values do you want to input: ";
-  cin >> n;
-  //Input the Values
-  cout << "Enter the values: ";
-  for (i = 0; i < n; i++) {
-    cin >> array[i];
-  }
-  
-  //Request for the value to be deleted
-  int key;
-  cout << "Enter the value you want to delete: ";
-  cin >> key;
-  
-  //Search for the key in the array
-  for (i = 0; i < n; i++) {
-    if (array[i] == key) {
-      n = n - 1;
-      for (int j = i; j < n; j++)
-        array[j] = array[j + 1];
-      break;
+    int n;
+    int i;
+    double array[50];
+
+    // Input the number of values
+    cout << "How many values do you want to input: ";
+    cin >> n;
+
+    // Input the values
+    cout << "Enter the values:";
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
     }
-  }
-  
-  for (int i = 0; i < n; i++)
-    cout << array[i] << " ";
+
+    // Request for the index to be deleted
+    int key;
+    cout << "Enter the index of the value you want to delete: ";
+    cin >> key;
+
+    // Check if the index is valid
+    if (key >= 0 && key < n) {
+
+        for (int i = key; i < n - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        n = n - 1;
+    } else {
+        cout << "Invalid index!!";
+    }
+
+    // Output the updated array
+    cout << "Updated array:";
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+
 }
